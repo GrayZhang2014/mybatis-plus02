4563fa4d753f7a2007357662182cffc54f20df53
zbl <15709611104@139.com>
2020-06-24 09:47:05 +0800

    0624 update

--- src/test/java/com/mp/test/SelectByWrapper.java
+++ src/test/java/com/mp/test/SelectByWrapper.java
@@ -2,7 +2,9 @@ package com.mp.test;
 
 
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -28,12 +30,36 @@ public class SelectByWrapper {
 	private UserMapper userMapper;
 	
 	@Test
+	public void slelectByallEq(){
+		
+		QueryWrapper<User>  queryWrapper=new QueryWrapper<User>();
+		Map<String,Object> map=new HashMap<String,Object>();
+		map.put("name","张帮龙");
+		map.put("age",31);
+		queryWrapper.allEq(map);
+		List<User>  list=userMapper.selectList(queryWrapper);
+	    list.forEach(System.out::println);
+		
+	}
+	
+	@Test
 	public void selectByWrapper(){
 		
 		//   名字中包含雨并且年龄小于40
 		//   name like '%雨%' and age<40
 		QueryWrapper<User> queryWrapper=new QueryWrapper<User>();
 		queryWrapper.like("name","雨").lt("age",40);
+        List<User> list=userMapper.selectList(queryWrapper);
+		list.forEach(System.out::println);
+	}
+	
+	@Test
+	public void selectByWrapper01(){
+		
+		//   名字中包含雨并且年龄小于40
+		//   name like '%雨%' and age<40
+		QueryWrapper<User> queryWrapper=new QueryWrapper<User>();
+		queryWrapper.likeRight("name", "王").and(wq->wq.lt("age", 40).or().isNotNull("email"));
         List<User> list=userMapper.selectList(queryWrapper);
 		list.forEach(System.out::println);
 	}
